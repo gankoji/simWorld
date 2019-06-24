2956ade3fb142d0657e7163c531407f680a6143e
Jake Bailey <asaxplayinghorse@gmail.com>
2019-06-24 02:59:25 -0700

    Moved to multiprocessing from threading for GIL reasons. Performance is actually good now, 33% reduction for MC cases.

--- simWorld/Main.py
+++ simWorld/Main.py
@@ -1,15 +1,30 @@
 import numpy as np
 import math
 import os
+import time
+import threading
+from multiprocessing import Process
 
 import h5py
 import Vehicle
 
+useThreading = True
+numRuns = 25
+dt = .001
+endTime = 10
 
+#dataStore = [0]*numRuns
+dataStore = []
+def runSim(j):
+    vehicle = Vehicle.Vehicle(dt, endTime)
+    for i in range(0, vehicle.simLength-1):
+        vehicle.update()
+        
+    dataStore.append(vehicle)
+    # print("Thread " + str(j) + " complete")
+
+start_time = time.time()
 scenario = "Base"
-numRuns = 10
-dt = .0005
-endTime = 100
 fileName = scenario + ".hdf5"
 if os.path.exists(fileName):
     os.remove(fileName)
@@ -19,19 +34,36 @@ f = h5py.File(fileName, "a",
 
 scenario = "/" + scenario
 np.random.seed(0)
-for j in range(0, numRuns):
-    nodeName = scenario + "/" + str(j) + "/VehicleData"
-    vehicle = Vehicle.Vehicle(dt, endTime)
-    for i in range(0, vehicle.simLength-1):
-        vehicle.update()
 
-    if nodeName in f:
-        f[nodeName].resize((f[nodeName].shape[0] 
-                            + vehicle.eom.data.shape[0], f[nodeName].shape[1]))
-        f[nodeName][-vehicle.eom.data.shape[0]:] = vehicle.eom.data
-    else:
-        f.create_dataset(nodeName, 
-                         chunks=True, data=vehicle.eom.data, 
-                         maxshape=(None, vehicle.eom.data.shape[1]))
+if useThreading:
+    threads = []
+    for j in range(0, numRuns):
+        threads.append(Process(target=runSim, args=(j,)))
+        threads[j].start()
+
+    for j in range(0, numRuns):
+        threads[j].join()
+
+else:
+    for j in range(0, numRuns):
+        runSim(j)
+        
+        
+# print(dataStore)
+# for j in range(0, numRuns):
+#     nodeName = scenario + "/" + str(j) + "/VehicleData"
+#     vehicle = dataStore[j]
+#     print(vehicle)
+
+    # if nodeName in f:
+    #     f[nodeName].resize((f[nodeName].shape[0] 
+    #                         + vehicle.eom.data.shape[0], f[nodeName].shape[1]))
+    #     f[nodeName][-vehicle.eom.data.shape[0]:] = vehicle.eom.data
+    # else:
+    #     f.create_dataset(nodeName, 
+    #                      chunks=True, data=vehicle.eom.data, 
+    #                      maxshape=(None, vehicle.eom.data.shape[1]))
 
 f.close()
+
+print("--- %s seconds ---" %(time.time() - start_time))
